1e5bcba4808c72c12cf8619e6a749f763e1d1e89
branch331 <bnguyen95@vt.edu>
2019-06-26 21:11:42 -0500

    added service tests

---
+++ rooster-api/rooster-tests/MockItem.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using roosterapi.Models;
+
+namespace roostertests
+{
+    public class MockItem : DatabaseItemBase
+    { 
+        [BsonElement("Name")]
+        public string MockItemName { get; set; }
+
+        [BsonElement("Description")]
+        public string MockItemDescription { get; set; }
+    }
+}
+
+
---
+++ rooster-api/rooster-tests/ServiceTests.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using roosterapi.Models;
+using roosterapi.Services;
+using roostertests;
+using Newtonsoft.Json;
+using Moq;
+
+namespace ServiceTests
+{
+    [TestFixture]
+    public class DashboardServiceTests
+    {
+        private MockService mockService;
+
+        [SetUp]
+        public void Setup()
+        {
+            IDatabaseSettingsBase mockDatabaseSettings = new DatabaseSettingsBase()
+            {
+                ConnectionString = "mongodb://localhost:27017",
+                DatabaseName = "mockDb"
+            };
+
+            mockService = new MockService(mockDatabaseSettings);
+        }
+
+        [Test]
+        public void TestCreateAndGet()
+        {
+            mockService.Create(new MockItem()
+            {
+                MockItemName = "Mock Name",
+                MockItemDescription = "Mock Description"
+            });
+
+            Assert.IsTrue(mockService.Get().ToArray()[0].MockItemName == "Mock Name");
+        }
+
+        [Test]
+        public void TestUpdateAndRemove()
+        {
+            var mockItem = mockService.Get().ToArray()[0];
+            mockItem.MockItemName = "Updated Name";
+
+            mockService.Update(mockItem.Id, mockItem);
+
+            Assert.IsTrue(mockService.Get().ToArray()[0].MockItemName == "Updated Name");
+
+            mockService.Remove(mockItem.Id);
+
+            Assert.IsTrue(mockService.Get().ToArray().Length == 0);
+        }
+    }
+}
