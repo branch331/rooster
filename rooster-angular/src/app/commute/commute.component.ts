dcfece48d5c56cfca6ede8677571ab85347e8a58
branch331 <bnguyen95@vt.edu>
2019-07-10 15:23:43 -0500

    Added map route image to commute item

--- rooster-angular/src/app/commute/commute.component.ts
+++ rooster-angular/src/app/commute/commute.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CommuteItem } from '../commuteItem';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-commute',
@@ -13,14 +15,15 @@ export class CommuteComponent implements OnInit {
   commuteOriginCoordinates: string;
   commuteDestinationCoordinates: string;
   commuteData: object;
+  commuteImage: Blob;
+  commuteImagePath: string;
+
   key = 'Ar7d_Frhf9pNH2QUoWIK95AmRObxnE0DyD2Qxxufv6be0sCu2tzX_V_mksU2A4lY';
 
   roosterapiBaseUrl = 'http://localhost:5000/api/commute/';
   bingDistanceApiBaseUrl = 'https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?';
 
-  //origins=30.428838,-97.659316&destinations=30.407825,-97.726570&travelMode=driving&key=Ar7d_Frhf9pNH2QUoWIK95AmRObxnE0DyD2Qxxufv6be0sCu2tzX_V_mksU2A4lY'
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.getCommuteItem();
@@ -33,6 +36,7 @@ export class CommuteComponent implements OnInit {
         this.commuteOriginCoordinates = this.commuteItem.commuteOriginLatitude + "," + this.commuteItem.commuteOriginLongitude;
         this.commuteDestinationCoordinates = this.commuteItem.commuteDestinationLatitude + "," + this.commuteItem.commuteDestinationLongitude;
         this.getCommuteData();
+        this.getCommuteImage();
       });
   }
 
@@ -49,4 +53,28 @@ export class CommuteComponent implements OnInit {
       });
   }
 
+  getCommuteImage(): void {
+    this.http.get('https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/Routes?wp.0=30.428838,-97.659316;46;A&wp.1=30.407825,-97.726570;46;B&key=Ar7d_Frhf9pNH2QUoWIK95AmRObxnE0DyD2Qxxufv6be0sCu2tzX_V_mksU2A4lY',
+                        { responseType: 'blob' })
+      .subscribe(commuteImage => {
+        this.commuteImage = commuteImage;
+        this.generateImageUrl(this.commuteImage);
+      });
+  }
+
+  generateImageUrl(imageBlob: Blob): void {
+    var reader = new FileReader();
+    var base64data;
+
+    reader.readAsDataURL(imageBlob);
+    reader.onloadend = () => {
+      base64data = reader.result;
+      this.setAndSanitizeImageUrl(base64data);
+    }
+  }
+
+  setAndSanitizeImageUrl(base64data: string): void {
+    this.commuteImagePath = base64data;
+    this.sanitizer.bypassSecurityTrustUrl(this.commuteImagePath);
+  }
 }
