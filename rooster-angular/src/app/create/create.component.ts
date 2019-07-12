7a524f9617720c5b5c905e86a6c8999adf6e7ca2
branch331 <bnguyen95@vt.edu>
2019-07-11 20:42:30 -0500

    HTTP requests can create weather, commute, and dashboard items from form

--- rooster-angular/src/app/create/create.component.ts
+++ rooster-angular/src/app/create/create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { DashboardItem } from '../dashboardItem';
 import { WeatherItem } from '../weatherItem';
+import { CommuteItem } from '../commuteItem';
 import { throwError } from 'rxjs';
 
 @Component({
@@ -10,11 +11,13 @@ import { throwError } from 'rxjs';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
-  model = new DashboardItem();
+  dashboardItem = new DashboardItem();
+  weatherItem = new WeatherItem();
+  commuteItem = new CommuteItem();
 
   dashboardItemTypes = ['Weather', 'Commute'];
 
-  private roosterapiUrl = 'http://localhost:5000/api/dashboard';
+  private roosterapiBaseUrl = 'http://localhost:5000/api/';
   
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json'})
@@ -24,20 +27,45 @@ export class CreateComponent {
 
   onSubmit() {
     console.log("submitted \n" 
-    + this.model.dashboardItemName + "\n"
-    + this.model.dashboardItemType);
+    + this.dashboardItem.dashboardItemName + "\n"
+    + this.dashboardItem.dashboardItemType);
 
-    if (this.model.dashboardItemType == "Weather") {
-      console.log("Weather item created.");
-    }
-    else if (this.model.dashboardItemType == "Commute") {
-      console.log("Commute item created.")
-    }
+    if (this.dashboardItem.dashboardItemType == "Weather") {
+      console.log("Weather item created: \n"
+      + this.weatherItem.weatherItemName + "\n"
+      + this.weatherItem.weatherItemLatitude + "\n"
+      + this.weatherItem.weatherItemLongitude);
 
-    this.http.post(this.roosterapiUrl, JSON.stringify(this.model), this.httpOptions)
-      .subscribe(response => alert("complete"));
+      this.http.post(this.roosterapiBaseUrl + "weather", this.weatherItem, this.httpOptions)
+        .subscribe(responseObject => {
+          let weatherItem = responseObject as WeatherItem;
+          this.dashboardItem.dashboardItemReferenceId = weatherItem.id;
+          this.createDashboardItem();
+        });
+    }
+    else if (this.dashboardItem.dashboardItemType == "Commute") {
+      console.log("Commute item created: \n"
+      + this.commuteItem.commuteItemName + "\n"
+      + this.commuteItem.commuteOriginName + "\n"
+      + this.commuteItem.commuteOriginLatitude + "\n"
+      + this.commuteItem.commuteOriginLongitude + "\n"
+      + this.commuteItem.commuteDestinationName + "\n"
+      + this.commuteItem.commuteDestinationLatitude + "\n"
+      + this.commuteItem.commuteDestinationLongitude);
+
+      this.http.post(this.roosterapiBaseUrl + "commute", this.commuteItem, this.httpOptions)
+      .subscribe(responseObject => {
+        let commuteItem = responseObject as CommuteItem;
+        this.dashboardItem.dashboardItemReferenceId = commuteItem.id;
+        this.createDashboardItem();
+      });
+    }
   }
 
+  createDashboardItem(): void {
+    this.http.post(this.roosterapiBaseUrl + 'dashboard', JSON.stringify(this.dashboardItem), this.httpOptions)
+      .subscribe(response => alert("complete"));
+  }
   /*
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
