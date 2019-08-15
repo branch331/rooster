4e458722b0c352c40375c0051ef463c7345d23f2
branch331 <34431991+branch331@users.noreply.github.com>
2019-08-14 23:45:36 -0500

    Added calendar to commute page; minor cleanup

--- rooster-angular/src/app/create/create.component.ts
+++ rooster-angular/src/app/create/create.component.ts
@@ -3,10 +3,12 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { DashboardItem } from '../dashboardItem';
 import { WeatherItem } from '../weatherItem';
 import { CommuteItem } from '../commuteItem';
+import { CalendarItem } from '../calendarItem';
 import { throwError } from 'rxjs';
 import { DashboardService } from '../dashboard.service'
 import { WeatherService } from '../weather.service'
 import { CommuteService } from '../commute.service'
+import { CalendarService } from '../calendar.service'
 
 @Component({
   selector: 'app-create',
@@ -17,12 +19,14 @@ export class CreateComponent {
   dashboardItem = new DashboardItem();
   weatherItem = new WeatherItem();
   commuteItem = new CommuteItem();
+  calendarItem = new CalendarItem();
 
-  dashboardItemTypes = ['Weather', 'Commute'];
+  dashboardItemTypes = ['Weather', 'Commute', 'Calendar'];
 
   constructor(private dashboardService: DashboardService,
               private weatherService: WeatherService,
-              private commuteSerivce: CommuteService) { }
+              private commuteService: CommuteService,
+              private calendarService: CalendarService) { }
 
   onSubmit() {
 
@@ -34,17 +38,25 @@ export class CreateComponent {
         });
     }
     else if (this.dashboardItem.dashboardItemType == "Commute") {
-      this.commuteSerivce.addCommuteItem(this.commuteItem)
+      this.commuteService.addCommuteItem(this.commuteItem)
         .subscribe(responseObject => {
           let commuteItem = responseObject as CommuteItem;
           this.dashboardItem.dashboardItemReferenceId = commuteItem.id;
           this.createDashboardItem();
         });
     }
+    else if (this.dashboardItem.dashboardItemType == "Calendar") {
+      this.calendarService.addCalendarItem(this.calendarItem)
+        .subscribe(responseObject => {
+          let calendarItem = responseObject as CalendarItem;
+          this.dashboardItem.dashboardItemReferenceId = calendarItem.id;
+          this.createDashboardItem();
+        })
+    }
   }
 
   createDashboardItem(): void {
     this.dashboardService.addDashboardItem(this.dashboardItem)
-      .subscribe(response => alert("complete"));
+      .subscribe(response => alert("Added dashboard item"));
   }
 }
